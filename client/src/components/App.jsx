ba7aa4e49168df679cecb25cfd3726287eba79a1
lydzb123 <lydiabaik@gmail.com>
2021-01-28 12:50:34 -0800

    Implement all working filters!

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,6 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 import EmployeeList from './EmployeeList.jsx';
-
 
 
 const AppWrapper = styled.div`
@@ -25,12 +24,12 @@ const Filters = styled.div`
     color: #fff;
     vertical-align: center;
 
-    #searchInput {
+    #search {
         height: 32px;
         width: 350px;
     }
 
-    #filterAge {
+    #age {
         width: 40px;
     }
 `;
@@ -45,40 +44,40 @@ class App extends React.Component {
             filteredEmployees: this.props.employee_data.employee_data
         }
         this.handleChange = this.handleChange.bind(this);
-        this.searchBy = this.searchBy.bind(this);
-
     }
 
     handleChange(event) {
-        if(event.target.id === 'searchInput'){
-            this.setState({search: event.target.value});
-        }
-        if(event.target.id === 'filterAge'){
-            this.setState({age: event.target.value});
-        }
 
-        if(event.target.id === 'filterDepartments'){
-            this.setState({department: event.target.value});
-        }
+        var state = this.state;
+        state[event.target.id] = event.target.value;
+        state.filteredEmployees = this.props.employee_data.employee_data;
+
+            if(state.department.toLowerCase() === "all") {
+                state.filteredEmployees = this.props.employee_data.employee_data
+            } else {
+                state.filteredEmployees =  this.props.employee_data.employee_data.filter(employee => {
+                    console.log('1, state: ', state);
+                    return employee.department === state.department;
+                });
+            }
+
+            if(state.search !== "") {
+                console.log('here', state);
+
+                state.filteredEmployees = state.filteredEmployees.filter(employee => {
+                    return employee.name.toLowerCase().includes(state.search.toLowerCase())
+                });
+            }
+
+            if(parseInt(state.age) !== 0 && state.age !== ""){
+                state.filteredEmployees = state.filteredEmployees.filter(employee => {
+                    return employee.age == state.age
+                });
+            }
+
+        this.setState({state});
     }
-
-    // componentDidMount(){
-    //     //get all employee data
-    // }
-
-
-    searchBy(name, age, department) {
-        // console.log('change detected', name, age, department);
-return
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if(this.state.search !== prevState.search ||
-            this.state.age !== prevState.search ||
-            this.state.department !== prevState.search) {
-                this.searchBy(this.state.search, this.state.age, this.state.department);
-        }
-    }
+    
 
     render() {
         const departments = [ ...new Set(this.props.employee_data.employee_data.map(employee => employee.department)) ];
@@ -90,20 +89,21 @@ return
             <h1>Employee Hub</h1>
 
             <Filters>
-                <input id="searchInput" type="text" placeholder="Search by Name..." onChange={this.handleChange} value={this.state.search}></input>
+                <input id="search" type="text" placeholder="Search by Name..." onChange={this.handleChange} value={this.state.search}></input>
                 
                 <label htmlFor="age">Age: 
-                    <input id="filterAge" type="number" placeholder="0" min="0" onChange={this.handleChange} value={this.state.age}></input>
+                    <input id="age" type="number" placeholder="0" min="0" onChange={this.handleChange} value={this.state.age}></input>
                 </label>
                 
                 <label htmlFor="departments">Dept: 
-                    <select name="departments" id="filterDepartments" onChange={this.handleChange}>
+                    <select name="departments" id="department" onChange={this.handleChange}>
                         <option value="all">View All</option>
                         {departments.map((dept, i) => (<option key={i} value={dept}>{dept}</option>))}
                     </select>
                 </label>
             </Filters>
             <EmployeeList employees={this.state.filteredEmployees}></EmployeeList>
+            
 
         </AppWrapper>
       )
